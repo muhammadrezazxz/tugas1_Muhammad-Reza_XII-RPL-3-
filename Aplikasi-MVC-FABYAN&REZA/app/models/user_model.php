d8e2ede984f52db6887c0d3848bc4e9832058c2e
reza <93992286+muhammadrezazxz@users.noreply.github.com>
2022-08-04 10:04:03 +0700

    Add files via upload

---
+++ Aplikasi-MVC-FABYAN&REZA/app/models/user_model.php
@@ -0,0 +1,8 @@
+<?php
+
+class User_model {
+    private $nama = 'Reza';
+    public function getUser() {
+        return $this->nama;
+    }
+};
---
+++ Aplikasi-MVC-FABYAN&REZA/app/views/template/footer.php
@@ -0,0 +1,9 @@
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+
+<script src="<?=BASEURL;?>/js/script.js"></script>
+
+</body>
+</html>
